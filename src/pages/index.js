f22c9299b83bea8c44892d2a5be66c72b526e747
김환 <kimhwan@gimhwan-ui-MacBookPro.local>
2021-01-22 12:31:47 +0900

    push to origin master deploy test

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,10 @@ export default function Home() {
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
       <Link to="/contact/">Contact</Link>
+      <p>What a world.</p>
+      <p>What a world.</p>
+      <p>What a world.</p>
+      <p>What a world.</p>
     </div>
   )
 }

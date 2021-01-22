aaa1158ee1e95b6b0c61f5edc85be7b3274c18f1
김환 <kimhwan@gimhwan-ui-MacBookPro.local>
2021-01-22 12:20:00 +0900

    gatsby-start-init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
+import { Link } from "gatsby"
 import React from "react"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <Header headerText="ddd" />
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <Link to="/contact/">Contact</Link>
+    </div>
+  )
 }
